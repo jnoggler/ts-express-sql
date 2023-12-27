0beb4a5a5c838ae2729505887045536b9c763953
Jakob Noggler <jakob.noggler@gmail.com>
2023-12-27 15:55:00 +0100

    basic logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import express from "express";
+import pinoHttp from "pino-http";
+
+import logger from "./logger";
 
 const app = express();
+
+app.use(pinoHttp({
+  logger,
+  customLogLevel: function (req, res, err) {
+    if (res.statusCode >= 400 && res.statusCode < 500) {
+      return 'warn'
+    } else if (res.statusCode >= 500 || err) {
+      return 'error'
+    }
+    return 'info'
+  },
+  customSuccessMessage: function (req, res) {
+    if (res.statusCode === 404) {
+      return `${req.method} resource not found`
+    }
+    return `${req.method} completed`
+  },
+  customErrorMessage: function (req, res, err) {
+    return 'request errored with status code: ' + res.statusCode
+  },
+}));
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -8,5 +33,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  logger.info(`Server is running at http://localhost:${port}`);
 });
---
+++ src/logger.ts
@@ -0,0 +1,7 @@
+import pino from "pino";
+
+const logger = pino({
+    // config comes here
+});
+
+export default logger;
