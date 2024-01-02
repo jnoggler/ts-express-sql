11e0832c8d9f2f4ef7da607552c4f079ceaa2c24
Jakob Noggler <jakob.noggler@gmail.com>
2024-01-02 13:35:46 +0100

    add config validation

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,22 @@
 import dotenv from 'dotenv';
+import { z } from 'zod';
+
+const configSchema = z.object({
+  port: z.number(),
+  jwtSecret: z.string(),
+  nodeEnv: z.string(),
+});
+
+type Config = z.infer<typeof configSchema>;
 
 dotenv.config();
 
-const config = {
-  port: process.env.PORT || 3000,
+const config: Config = {
+  port: Number(process.env.PORT) || 3000,
   jwtSecret: process.env.JWT_SECRET || '', // TODO: what to do here?
   nodeEnv: process.env.NODE_ENV || 'development',
 };
+
+configSchema.parse(config);
 
 export default config;
