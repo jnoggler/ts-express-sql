073fdd159dd611c35e8bf8e2cc72f66ca23784a9
Jakob Noggler <jakob.noggler@gmail.com>
2024-01-02 15:09:39 +0100

    config error handling and logging without pino

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
+/*
+ * NOTE: log statements in this file will not be logged by pino, because it is not yet initialized.
+ */
+
 const configSchema = z.object({
   port: z.number(),
   jwtSecret: z.string(),
@@ -13,10 +17,19 @@ dotenv.config();
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
-  jwtSecret: process.env.JWT_SECRET || '', // TODO: what to do here?
+  jwtSecret: process.env.JWT_SECRET as string, // we do not want a default value here, to make sure that the app crashes if the secret is not set
   nodeEnv: process.env.NODE_ENV || 'development',
 };
 
-configSchema.parse(config);
+try {
+  configSchema.parse(config);
+} catch (error) {
+  console.error('config validation error');
+  throw error;
+}
+
+if (config.nodeEnv !== 'production') {
+  console.log('config', config);
+}
 
 export default config;
